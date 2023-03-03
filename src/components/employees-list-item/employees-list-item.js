ce2f9aff694bffa745127b068ef14c77ac5ace38
Lida Aupova <lida.aupova@gmail.com>
2023-03-03 20:27:19 +0300

    Функционал учёта сотрудников и изменение сотояния increase и rise

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,55 +1,32 @@
-import { Component } from 'react';
 import './employees-list-item.css';
 
-class EmployeesListItem extends Component {
+const EmployeesListItem = (props) => {
+    const {name, salary, onDelete, onToggleProp, increase, rise} = props;
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            increase: false,
-            promotion: false
-        }
-    }
+    return (
+        <li className={"list-group-item d-flex justify-content-between" + (increase ? " increase" : "") + (rise ? " like" : "")}>
+            <span 
+                onClick={onToggleProp}
+                data-toggle="rise"
+                className="list-group-item-label">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+            <div className="d-flex justify-content-center align-items-center">
+                <button type="button" 
+                        className="btn-cookie btn-sm"
+                        onClick={onToggleProp}
+                        data-toggle="increase">
+                    <i className="fas fa-cookie-bite"></i>
+                </button>
 
-    onIncrease = () => {
-        this.setState(({increase}) => ({
-            increase: !increase
-        }))
-    }
-
-    forPromotion = () => {
-        this.setState(({promotion}) => ({
-            promotion: !promotion
-        }))
-    }
-
-    render() {
-        const {name, salary, onDelete} = this.props;
-        const {increase, promotion} = this.state;
-
-        return (
-            <li className={"list-group-item d-flex justify-content-between" + (increase ? " increase" : "") + (promotion ? " like" : "")}>
-                <span 
-                    onClick={this.forPromotion} 
-                    className="list-group-item-label">{name}</span>
-                <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
-                <div className="d-flex justify-content-center align-items-center">
-                    <button type="button" 
-                            className="btn-cookie btn-sm"
-                            onClick={this.onIncrease}>
-                        <i className="fas fa-cookie-bite"></i>
-                    </button>
-    
-                    <button type="button" 
-                            className="btn-trash btn-sm"
-                            onClick={onDelete}>
-                        <i className="fas fa-trash"></i>
-                    </button>
-                    <i className="fas fa-star"></i>
-                </div>
-            </li>
-        );
-    }
+                <button type="button" 
+                        className="btn-trash btn-sm"
+                        onClick={onDelete}>
+                    <i className="fas fa-trash"></i>
+                </button>
+                <i className="fas fa-star"></i>
+            </div>
+        </li>
+    );
 };
 
 export default EmployeesListItem;
